01a0088f5a6a130904bd565f0e56d40ec6060530
RandyLiu6410 <wert6410@gmail.com>
2023-11-16 01:29:10 +0800

    feat: update animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,33 @@
-import clsx from 'clsx';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import Heading from '@theme/Heading';
+import clsx from "clsx";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import Heading from "@theme/Heading";
 
-import styles from './index.module.css';
-import TypingTypography, { TypingTypographyItem } from '../components/typing-typography';
+import styles from "./index.module.css";
+import { TypeAnimation } from "react-type-animation";
 
 function HomepageHeader() {
-  const typingItems: Array<TypingTypographyItem> = [
-    {
-      value: 'Learn it.',
-      duration: 2000
-    },
-    {
-      value: 'Note it.',
-      duration: 2000
-    },
-    {
-      value: 'And share it.',
-      duration: 2000
-    }
-  ]
-
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          <TypingTypography items={typingItems} />
+          {/* <TypingTypography items={typingItems} /> */}
+          <TypeAnimation
+            sequence={[
+              // Same substring at the start will only be typed out once, initially
+              "Learn it.",
+              1000, // wait 1s before replacing "Mice" with "Hamsters"
+              "Note it.",
+              1000,
+              "And share it.",
+              1000
+            ]}
+            wrapper="span"
+            speed={5}
+            deletionSpeed={15}
+            style={{ fontSize: "1em", display: "inline-block" }}
+            repeat={Infinity}
+          />
         </Heading>
       </div>
     </header>
@@ -34,11 +35,10 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
 
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}>
+    <Layout title={`Hello from ${siteConfig.title}`}>
       <HomepageHeader />
     </Layout>
   );
