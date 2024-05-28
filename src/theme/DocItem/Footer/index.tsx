6427adde0fbd514e46b3ccac197c1d2bd8529f04
RandyLiu6410 <47877698+RandyLiu6410@users.noreply.github.com>
2024-05-28 23:27:37 +0800

    feat: wit (#38)

--- src/theme/DocItem/Footer/index.tsx
+++ src/theme/DocItem/Footer/index.tsx
@@ -3,10 +3,13 @@ import { useDoc } from "@docusaurus/theme-common/internal";
 import Footer from '@theme-original/DocItem/Footer';
 import GiscusComponent from '@site/src/components/GiscusComponent';
 import useIsBrowser from '@docusaurus/useIsBrowser';
+import type FooterType from "@theme/DocItem/Footer";
+import type { WrapperProps } from "@docusaurus/types";
+type Props = WrapperProps<typeof FooterType>;
 
 import styles from './footer.module.css'
 
-export default function FooterWrapper(props) {
+export default function FooterWrapper(props: Props): JSX.Element {
   const isBrowser = useIsBrowser();
 
   const { metadata } = useDoc();
